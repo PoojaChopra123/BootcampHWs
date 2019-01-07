60b9c6f0fa0b11b9ab70412d3c22d5bd4faf6587
Pooja Chopra <poojachopra2018@u.northwestern.edu>
2019-01-07 14:20:27 -0600

    hw commit

---
+++ PythonAPI/06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+api_key = "1b5fd1e75fa0431f1f4fafe933f7a954"
---
+++ Social Analytics (Twitter)/07-Social-Analytics/Instructions/config.py
@@ -0,0 +1,11 @@
+# Twitter API Keys
+#consumer_key = "14mdGriY7kAGKcmAUyAl5Vx6G"
+#consumer_secret = "JnLdih0CCosI4pmZ9oUthSUl4A69HSiyuKZkIwbkfb9cv8CQyE"
+#access_token = "918518798558281728-MSOvD50yBBJn6MfmQwGVHbGWbYpTsTc"
+#access_token_secret = "mvmf036vz0jEdWFzqRwFl77isE9Nu9KoWVJeHDWlL8jAJ"
+
+# Twitter API Keys
+consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
+consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
+access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
+access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
